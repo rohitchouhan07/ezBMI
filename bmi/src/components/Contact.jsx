57773a5c2035784c8df02e6a55cd9e05ac69e74b
Rohit <rohitchouhan07@gmail.com>
2020-09-22 19:06:19 +0530

    Added bootstrap styling

--- bmi/src/components/Contact.jsx
+++ bmi/src/components/Contact.jsx
@@ -20,16 +20,22 @@ export class Contact extends Component {
     render() {
         const {userId, title, body} = this.state;
         return (
-            <div>
+            <div class="form-group">
                 <form onSubmit={this.submitHandler}>
+                    <h4>Name</h4>
                     <div>
-                        <input type="text" name="userId" value={userId} onChange={this.changeHandler}/>
+                        <input class="form-control" type="text" name="userId" value={userId} onChange={this.changeHandler}/>
                     </div>
+                    <br></br>
+                    <h4>Email</h4>
                     <div>
-                        <input type="text" name="title" value={title} onChange={this.changeHandler}/>
+                        <input  class="form-control" type="text" name="title" value={title} onChange={this.changeHandler}/>
                     </div>
+                    <br>
+                    </br>
+                    <h4>Query</h4>
                     <div>
-                        <input type="text" name="body" value={body} onChange={this.changeHandler} />
+                        <input  class="form-control" type="text" name="body" value={body} onChange={this.changeHandler} />
                     </div>
                     <button type="submit">Submit</button>
                 </form>
